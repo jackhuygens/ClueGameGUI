9eacd5e7f3a582a31cad3f9a10a4ac76692f020a
Jack Huygens <jack.huygens@clear.net.nz>
2016-08-16 17:40:47 +1200

    added comments in UIManager

--- src/UI/UIManager.java
+++ src/UI/UIManager.java
@@ -22,23 +22,27 @@ public class UIManager {
 		
 		this.main = main;
 		
+		// create frame
 		frame = new JFrame("Java SWING Examples");
 		frame.setSize(600,600);
 		frame.setTitle("Cluedo");
 		frame.setLayout(new BorderLayout());
-
 		frame.addWindowListener(new WindowAdapter() {
 		public void windowClosing(WindowEvent windowEvent){ System.exit(0); }});    
 		
+		// create menus
 		new MenuManager(frame);
 		
+		// create board panel 
 		boardController =  new BoardController(new BoardView(frame), main.board);
 		
+		// create side bar panel
 		JPanel handPanel = new JPanel();
 		handPanel.setBackground(Color.LIGHT_GRAY);
 		handPanel.setPreferredSize(new Dimension(150, 100));
 		frame.add(handPanel, BorderLayout.WEST);
 		
+		// create all other components, and store then in the side bar
 		handController =  new HandController(new HandView(handPanel), main.activePlayer);
 		ChoiceController =  new ChoiceController(new ChoiceView(handPanel), main.input);
 		DiceController =  new DiceController(new DiceView(handPanel), main.input);
